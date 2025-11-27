a6f863b8473ffc7b0bf4de8501410e79e340bd1d
Sesarch <sesarch@yahoo.com>
2025-11-26 21:22:01 -0800

    Refactor Home component structure and remove Navigation

--- main-site/src/app/page.tsx
+++ main-site/src/app/page.tsx
@@ -1,20 +1,15 @@
-import Link from 'next/link'
-import Navigation from '@/components/Navigation'
 import Hero from '@/components/Hero'
 import Features from '@/components/Features'
 import Platforms from '@/components/Platforms'
 import Pricing from '@/components/Pricing'
-import Footer from '@/components/Footer'
 
 export default function Home() {
   return (
-    <main className="min-h-screen">
-      <Navigation />
+    <>
       <Hero />
-      <Platforms />
       <Features />
+      <Platforms />
       <Pricing />
-      <Footer />
-    </main>
+    </>
   )
 }
